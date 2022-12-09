e428cb0662e81bc8ca8faa046d01d02a14ae6462
soomin-world <ojudve123@gmail.com>
2022-12-09 23:50:02 +0900

    extra reducer연습

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+// src/App.jsx
+
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { __getTodos } from "./redux/modules/todosSlice";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { isLoading, error, todos } = useSelector((state) => state.todos);
+
+  useEffect(() => {
+    dispatch(__getTodos());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>로딩 중....</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
